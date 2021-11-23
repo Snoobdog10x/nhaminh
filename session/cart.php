71cb3434ab6c63a17713ca8b5f3ef5d7375ae7f5
Thanh Duy Nguyen <67373941+Snoobdog10x@users.noreply.github.com>
2021-11-23 17:39:28 +0700

    update cart

--- session/cart.php
+++ session/cart.php
@@ -1,49 +1,51 @@
 <?php
+require_once("./Class/OrderItem.php");
 session_start();
-require_once("../Class/OrderItem.php");
+
 function get_Item($productID, $phoneCom, $phoneModel)
 {
     foreach ($_SESSION['cart'] as $item) {
-        if ($item->isOrderItem($productID, $phoneCom, $phoneModel)) {
-            return $item;
-        }
+       if( $item->isOrderItem($productID,$phoneCom,$phoneModel)){
+           return $item;
+       }
     }
     return null;
 }
-if (isset($_SESSION['cart'])) {
-    //update item
-    if (isset($_REQUEST['index']) && isset($_REQUEST['quantity'])) {
-        $index = $_REQUEST['index'];
-        $_SESSION['cart'][$index]->quantity = $_REQUEST['quantity'];
-        if (isset($_SERVER["HTTP_REFERER"])) {
-            header("Location: " . $_SERVER["HTTP_REFERER"]);
-        }
-    }
-    //delete item
-    if (isset($_REQUEST['index']) && !isset($_REQUEST['quantity'])) {
-        $index = $_REQUEST['index'];
-        array_splice($_SESSION['cart'], $index, 1);
-        if (isset($_SERVER["HTTP_REFERER"])) {
-            header("Location: " . $_SERVER["HTTP_REFERER"]);
-        }
-    }
-    //add cart
-    if (isset($_REQUEST['productID'])) {
-        $productID = $_REQUEST['productID'];
-        $phoneCom = $_REQUEST['phoneCom'];
-        $phoneModel = $_REQUEST['phoneModel'];
-        $price = $_REQUEST['price'];
-        $item = get_Item($productID,$phoneCom,$phoneModel);
-        if($item==null){
-            $item=new OrderItem($productID,$phoneCom,$phoneModel,1,$price);
-            array_push($_REQUEST['cart'],$item);
-        }else{
-            $item->quantity+=1;
-        }
-        if (isset($_SERVER["HTTP_REFERER"])) {
-            header("Location: " . $_SERVER["HTTP_REFERER"]);
-        }
-    }
-} else {
+if (!isset($_SESSION['cart'])) {
+
     $_SESSION['cart'] = array();
+}
+
+//update item
+if (isset($_REQUEST['index']) && isset($_REQUEST['quantity'])) {
+    $index = $_REQUEST['index'];
+    $_SESSION['cart'][$index]->quantity = $_REQUEST['quantity'];
+    if (isset($_SERVER["HTTP_REFERER"])) {
+        header("Location: " . $_SERVER["HTTP_REFERER"]);
+    }
+}
+//delete item
+if (isset($_REQUEST['index']) && !isset($_REQUEST['quantity'])) {
+    $index = $_REQUEST['index'];
+    array_splice($_SESSION['cart'], $index, 1);
+    if (isset($_SERVER["HTTP_REFERER"])) {
+        header("Location: " . $_SERVER["HTTP_REFERER"]);
+    }
+}
+//add cart
+if (isset($_REQUEST['productID'])) {
+    $productID = $_REQUEST['productID'];
+    $phoneCom = $_REQUEST['phoneCom'];
+    $phoneModel = $_REQUEST['phoneModel'];
+    $price = $_REQUEST['price'];
+    $item = get_Item($productID, $phoneCom, $phoneModel);
+    if ($item == null) {
+        $item = new OrderItem($productID, $phoneCom, $phoneModel, 1, $price);
+        array_push($_SESSION['cart'], $item);
+    } else {
+        $item->quantity += 1;
+    }
+    if (isset($_SERVER["HTTP_REFERER"])) {
+        header("Location: " . $_SERVER["HTTP_REFERER"]);
+    }
 }
--- session/testcart.php
+++ session/testcart.php
@@ -1,7 +1,13 @@
+<?php
+session_start();
+var_dump($_SESSION['cart']);
+?>
 <form method="GET" action="cart.php">
     <input type="hidden" name="productID" value="id" id="">
     <input type="hidden" name="phoneCom" value="samsung" id="">
     <input type="hidden" name="phoneModel" value="j6" id="">
     <input type="hidden" name="price" value="500" id="">
-    <button type="submit"></button>
+    <button type="submit"> add</button>
 </form>
+<button onclick="location.href = 'cart.php?index=0&&quantity=100';">update</button>
+<button onclick="location.href = 'cart.php?index=0';">delete</button>
