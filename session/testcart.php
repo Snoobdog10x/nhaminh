6705b4ace53b26b46cd2d08a3e6887537fb7c3c9
Thanh Duy Nguyen <67373941+Snoobdog10x@users.noreply.github.com>
2021-11-23 16:52:34 +0700

    Create testcart.php

---
+++ session/testcart.php
@@ -0,0 +1,7 @@
+<form method="GET" action="cart.php">
+    <input type="hidden" name="productID" value="id" id="">
+    <input type="hidden" name="phoneCom" value="samsung" id="">
+    <input type="hidden" name="phoneModel" value="j6" id="">
+    <input type="hidden" name="price" "500" id="">
+    <button type="submit"></button>
+</form>
